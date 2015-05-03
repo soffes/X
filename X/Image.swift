d6c53b25ce3adb4369fecd9f6ef7f427c3c7855a
Sam Soffes <sam@soff.es>
2015-05-03 15:41:36 -0400

    Add `init?(named name: String, inBundle bundle: NSBundle?)` to Image

--- X/Image.swift
+++ X/Image.swift
@@ -22,6 +22,16 @@
 		public convenience init?(CGImage cgImage: CGImageRef) {
 			self.init(CGImage: cgImage, size: CGSizeZero)
 		}
+
+		public convenience init?(named name: String, inBundle bundle: NSBundle?) {
+			#if os(iOS)
+				self.init(named: name, inBundle: bundle, compatibleWithTraitCollection: nil)
+			#else
+				let b = bundle ?? NSBundle.mainBundle()
+				let path = b.pathForImageResource(name) ?? ""
+				self.init(contentsOfFile: path)
+			#endif
+		}
 	}
  #endif
 
