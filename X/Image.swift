186eab1968730da595b0ae281f28b97425b9c23d
Sam Soffes <sam@soff.es>
2015-05-03 15:46:04 -0400

    Make `Image.init?(named name: String, inBundle bundle: NSBundle?)` work on iOS

--- X/Image.swift
+++ X/Image.swift
@@ -22,17 +22,19 @@
 		public convenience init?(CGImage cgImage: CGImageRef) {
 			self.init(CGImage: cgImage, size: CGSizeZero)
 		}
-
-		public convenience init?(named name: String, inBundle bundle: NSBundle?) {
-			#if os(iOS)
-				self.init(named: name, inBundle: bundle, compatibleWithTraitCollection: nil)
-			#else
-				let b = bundle ?? NSBundle.mainBundle()
-				let path = b.pathForImageResource(name) ?? ""
-				self.init(contentsOfFile: path)
-			#endif
-		}
 	}
  #endif
 
 public typealias Image = ImageType
+
+extension Image {
+	public convenience init?(named name: String, inBundle bundle: NSBundle?) {
+		#if os(iOS)
+			self.init(named: name, inBundle: bundle, compatibleWithTraitCollection: nil)
+		#else
+			let b = bundle ?? NSBundle.mainBundle()
+			let path = b.pathForImageResource(name) ?? ""
+			self.init(contentsOfFile: path)
+		#endif
+	}
+}
