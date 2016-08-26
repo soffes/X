5d2d0d4e6c172010be3f264ffdf655bbc645d05a
Sam Soffes <sam@soff.es>
2016-08-26 10:52:42 -0500

    Add support for dynamic type

---
+++ Sources/FontWeight.swift
@@ -0,0 +1,55 @@
+//
+//  FontWeight.swift
+//  X
+//
+//  Created by Sam Soffes on 8/26/16.
+//  Copyright © 2016 Sam Soffes. All rights reserved.
+//
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+	import UIKit
+#elseif os(OSX)
+	import CoreGraphics
+#endif
+
+public enum FontWeight {
+	case ultraLight
+	case thin
+	case light
+	case regular
+	case medium
+	case semibold
+	case bold
+	case heavy
+	case black
+
+	#if os(iOS) || os(tvOS) || os(watchOS)
+	public var weight: CGFloat {
+		switch self {
+		case .ultraLight: return UIFontWeightUltraLight
+		case .thin: return UIFontWeightThin
+		case .light: return UIFontWeightLight
+		case .regular: return UIFontWeightRegular
+		case .medium: return UIFontWeightMedium
+		case .semibold: return UIFontWeightSemibold
+		case .bold: return UIFontWeightBold
+		case .heavy: return UIFontWeightHeavy
+		case .black: return UIFontWeightBlack
+		}
+	}
+	#else
+	public var weight: CGFloat {
+		switch self {
+		case .ultraLight: return -0.8
+		case .thin: return -0.6
+		case .light: return -0.4
+		case .regular: return 0.0
+		case .medium: return 0.23
+		case .semibold: return 0.3
+		case .bold: return 0.4
+		case .heavy: return 0.56
+		case .black: return 0.62
+		}
+	}
+	#endif
+}
---
+++ Sources/TextStyle.swift
@@ -0,0 +1,83 @@
+//
+//  TextStyle.swift
+//  X
+//
+//  Created by Sam Soffes on 8/26/16.
+//  Copyright © 2016 Sam Soffes. All rights reserved.
+//
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+	import UIKit
+#elseif os(OSX)
+	import AppKit
+#endif
+
+public enum TextStyle {
+	case title1
+	case title2
+	case title3
+	case headline
+	case subheadline
+	case body
+	case footnote
+	case caption1
+	case caption2
+	case callout
+
+	#if os(iOS) || os(tvOS) || os(watchOS)
+	public var textStyle: String {
+		switch self {
+		case .title1: return UIFontTextStyleTitle1
+		case .title2: return UIFontTextStyleTitle2
+		case .title3: return UIFontTextStyleTitle3
+		case .headline: return UIFontTextStyleHeadline
+		case .subheadline: return UIFontTextStyleSubheadline
+		case .body: return UIFontTextStyleBody
+		case .footnote: return UIFontTextStyleFootnote
+		case .caption1: return UIFontTextStyleCaption1
+		case .caption2: return UIFontTextStyleCaption2
+		case .callout: return UIFontTextStyleCallout
+		}
+	}
+	#endif
+
+	public var fontWeight: FontWeight {
+		switch self {
+		case .title1: return .light
+		case .headline: return .semibold
+		default: return .regular
+		}
+	}
+
+	public var fontSize: CGFloat {
+		switch self {
+		case .title1: return 28
+		case .title2: return 22
+		case .title3: return 20
+		case .headline: return 17
+		case .subheadline: return 15
+		case .body: return 17
+		case .footnote: return 13
+		case .caption1: return 12
+		case .caption2: return 11
+		case .callout: return 16
+		}
+	}
+}
+
+
+extension Font {
+	public static func preferredFontForTextStyle(textStyle: TextStyle) -> Font {
+		#if os(iOS) || os(tvOS) || os(watchOS)
+			return Font.preferredFontForTextStyle(textStyle.textStyle)
+		#else
+			var descriptor = NSFont.systemFontOfSize(textStyle.fontSize).fontDescriptor
+			descriptor = descriptor.fontDescriptorByAddingAttributes([
+				NSFontTraitsAttribute: [
+					NSFontWeightTrait: textStyle.fontWeight.weight
+				]
+			])
+			return NSFont(descriptor: descriptor, size: textStyle.fontSize)!
+		#endif
+	}
+}
