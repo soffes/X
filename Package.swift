69c28ff29c026f35b9397520db131c3184749051
Sam Soffes <sam@soff.es>
2019-09-28 09:44:21 -0700

    Deployment target

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "X",
+    platforms: [
+        .macOS("10.9"),
+        .iOS("9.0"),
+        .tvOS("9.0"),
+        .watchOS("2.0")
+    ],
     products: [
         .library(name: "X", targets: ["X"])
     ],
